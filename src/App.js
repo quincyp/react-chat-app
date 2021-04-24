3d1d1812130eb8c1bb05af1cee5481fa8dd32431
qtpham <quincytpham@gmail.com>
2021-04-23 17:05:12 -0700

    Fixed chatroom subtitle username spacing issue, switched to login form system, tested with multiple users

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
     return(
         <ChatEngine
             height="100vh"
             projectID="51e42095-efb3-49ce-8e8b-7c8fd830d4d4"
-            userName="quincy"
-            userSecret="test"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
